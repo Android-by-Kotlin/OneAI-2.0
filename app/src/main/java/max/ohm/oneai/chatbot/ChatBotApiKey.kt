7e2325d0c035d4d017f60935197640d04e1baace
Om Prakash <149907836+maxohm1@users.noreply.github.com>
2025-12-20 22:06:43 +0530

    change in chatbot

--- app/src/main/java/max/ohm/oneai/chatbot/ChatBotApiKey.kt
+++ app/src/main/java/max/ohm/oneai/chatbot/ChatBotApiKey.kt
@@ -7,4 +7,7 @@ package max.ohm.oneai.chatbot
 // new api key of gemini
 //internal val GEMINI_API_KEY = "AIzaSyAIosBXp80S8Vsdiq4CKB1AxhPLGwb01RE" // Gemini API Key
 
-internal val GEMINI_API_KEY = "AIzaSyCrWF3Wq9tN1-0IsWRoVC0MVhE0kS0YXeY" // Gemini API Key
+// internal val GEMINI_API_KEY = "AIzaSyCrWF3Wq9tN1-0IsWRoVC0MVhE0kS0YXeY" // Gemini API Key
+
+internal val GEMINI_API_KEY = "AIzaSyCwPvy-prP2Yq4kYTYFalQRYpynyT3hNVc" // Gemini API Key
+
